68f8617ebad3445a36a4ad891ce8cdcade85d3f6
Stephan <stephan.startsev@gmail.com>
2019-11-06 14:14:20 +0300

    6 HW

---
+++ sixthHW/webpack.dev.config.js
@@ -0,0 +1,76 @@
+/* eslint-disable */
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '',
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.pug$/i,
+        use: {
+          loader: "pug-loader",
+          query: {}, 
+        }
+      },
+      {
+        test: /\.ts(x?)$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      },
+      {
+        test: /\.(sa|sc|c)ss$/, 
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
+            }
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new HtmlWebpackPlugin({ filename: `index.html`, template: 'src/views/index.pug'})
+  ]
+}
