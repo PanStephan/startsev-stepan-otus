74fb36432c7e4cead302966348b723c816acb742
Stephan <stephan.startsev@gmail.com>
2019-12-28 17:36:46 +0300

    split axios req

--- weatherApp/src/services/getInfo.ts
+++ weatherApp/src/services/getInfo.ts
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from '../axios/weatherReq'
 import {WEATHERSTACK_KEY} from '../config/variables'
  
 const weatherInfo = async (query) => {
-  return await axios.get(`http://api.weatherstack.com/current?access_key=${WEATHERSTACK_KEY}&query=${query}`)
+  return await axios.get(`current?access_key=${WEATHERSTACK_KEY}&query=${query}`)
     .then(res => res.data)
     .catch(err => console.log(err))
 }
